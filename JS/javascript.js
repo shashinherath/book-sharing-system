f639fa4dabbe750708bec13c90f0bc4564677991
ShashinHerath <116209315+ShashinHerath@users.noreply.github.com>
2023-07-23 23:35:57 +0530

    Add Success Stories

--- JS/javascript.js
+++ JS/javascript.js
@@ -4,3 +4,25 @@ $(document).ready(function () {
     time: 1200,
   });
 });
+
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
