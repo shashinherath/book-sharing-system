14893e80b5710d7101bdcac9180c744cb2cee328
ShashinHerath <116209315+ShashinHerath@users.noreply.github.com>
2023-08-05 23:08:23 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,318 @@
+<?php
+session_start();
+
+include("database.php");
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+
+    if (!empty($email) && !empty($password) && !is_numeric($email) && $password == $cpassword) {
+        $query = "INSERT INTO user (name, email, phone, password, cpassword) VALUES ('$name','$email','$phone','$password','$cpassword')";
+        if (mysqli_query($con, $query)) {
+            header("location: login.php");
+        } else {
+            echo "<script type='text/javascript'> alert('Error: " . mysqli_error($con) . "' ) </script>";
+        }
+    } else {
+        echo "<script type='text/javascript'> alert('Please enter some valid information')</script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>BookBridge</title>
+
+    <link rel="stylesheet" href="CSS/style.css" />
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    />
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="assets/css/slicknav.css" />
+    <link rel="stylesheet" href="assets/css/animate.min.css" />
+    <link rel="stylesheet" href="assets/css/price_rangs.css" />
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+    <link rel="stylesheet" href="assets/css/themify-icons.css" />
+    <link rel="stylesheet" href="assets/css/slick.css" />
+    <link rel="stylesheet" href="assets/css/nice-select.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <!-- Navigation bar -->
+
+    <header>
+      <div class="header-area">
+        <div class="main-header">
+          <div class="header-top">
+            <div class="container">
+              <div class="row">
+                <div class="col-xl-12">
+                  <div
+                    class="d-flex justify-content-between align-items-center flex-sm"
+                  >
+                    <div class="header-info-left d-flex align-items-center">
+                      <div class="logo">
+                        <a href="index.php"
+                          ><img src="Images/logo.png" alt="BookBridge"
+                        /></a>
+                      </div>
+
+                      <form action="#" class="form-box">
+                        <input
+                          type="text"
+                          name="Search"
+                          placeholder="Search book by author or publisher"
+                        />
+                        <div class="search-icon">
+                          <i class="bi bi-search"></i>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="header-info-right d-flex align-items-center">
+                      <ul>
+                        <li>
+                          <a href="register.html" class="btn header-btn"
+                            >Register</a
+                          >
+                          <a href="login.php" class="btn header-btn">Login</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="header-bottom header-sticky">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-xl-12">
+                  <div class="logo2">
+                    <a href="index.php"
+                      ><img src="Images/logo.png" alt="BookBridge"
+                    /></a>
+                  </div>
+
+                  <div class="main-menu text-center d-none d-lg-block">
+                    <nav>
+                      <ul id="navigation">
+                        <li><a href="index.php">Home</a></li>
+                        <li>
+                          <a href="#">Categories</a>
+                          <ul class="submenu">
+                            <li>
+                              <a href="guidline.php">Donation Guidelines</a>
+                            </li>
+                            <li>
+                              <a href="organizations.php"
+                                >Recipient Organizations</a
+                              >
+                            </li>
+                            <li><a href="Stories.php">Success Stories</a></li>
+                            <li>
+                              <a href="resources.php">Educational Resources</a>
+                            </li>
+                            <li>
+                              <a href="forums.php">Community Forums</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li><a href="books.php">Books</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+
+                <div class="col-xl-12">
+                  <div class="mobile_menu d-block d-lg-none">
+                    <div class="slicknav_menu"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <br />
+
+    <!-- Register form -->
+
+    <main class="login-bg">
+      <div class="register-form-area">
+        <div class="register-form text-center">
+          <div class="register-heading">
+            <span>Register</span>
+            <p>Create your account to get full access</p>
+          </div>
+            <form method="post">
+          <div class="input-box">
+            <div class="single-input-fields">
+              <label>Full name</label>
+              <input type="text" name="name" placeholder="Enter full name" required/>
+            </div>
+            <div class="single-input-fields">
+              <label>Email Address</label>
+              <input type="email" name="email" placeholder="Enter email address" required/>
+            </div>
+            <div class="single-input-fields">
+              <label>Phone Number</label>
+              <input type="tel" name="phone" placeholder="Enter phone number" required/>
+            </div>
+            <div class="single-input-fields">
+              <label>Password</label>
+              <input type="password" name="password" placeholder="Enter Password" required/>
+            </div>
+            <div class="single-input-fields">
+              <label>Confirm Password</label>
+              <input type="password" name="cpassword" placeholder="Confirm Password" required/>
+            </div>
+          </div>
+
+          <div class="register-footer">
+            <p>Already have an account? <a href="login.php"> Login</a> here</p>
+            <button class="submit-btn3" type="submit">Sign Up</button>
+          </div>
+            </form>
+        </div>
+      </div>
+    </main>
+
+    <!-- Footer -->
+
+    <footer>
+      <div class="footer-wrappper section-bg">
+        <div class="footer-area footer-padding">
+          <div class="container">
+            <div class="row justify-content-between">
+              <div class="col-xl-3 col-lg-5 col-md-4 col-sm-6">
+                <div class="single-footer-caption mb-50">
+                  <div class="single-footer-caption mb-30">
+                    <div class="footer-logo mb-25">
+                      <a href="index.php"
+                        ><img src="Images/logo.png" alt="BookBridge"
+                      /></a>
+                    </div>
+                    <div class="footer-tittle">
+                      <div class="footer-pera">
+                        <p>
+                          Sowing Seeds of Literacy, Cultivating Bright Futures!
+                        </p>
+                      </div>
+                    </div>
+
+                    <div class="footer-social">
+                      <a href="#"><i class="bi bi-facebook"></i></a>
+                      <a href="#"><i class="bi bi-instagram"></i></a>
+                      <a href="#"><i class="bi bi-linkedin"></i></a>
+                      <a href="#"><i class="bi bi-youtube"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
+                <div class="single-footer-caption mb-50">
+                  <div class="footer-tittle">
+                    <h4>Book Category</h4>
+                    <ul>
+                      <li><a href="#">History</a></li>
+                      <li><a href="#">Horror - Thriller</a></li>
+                      <li><a href="#">Love Stories</a></li>
+                      <li><a href="#">Science Fiction</a></li>
+                      <li><a href="#">Business</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                <div class="single-footer-caption mb-50">
+                  <div class="footer-tittle">
+                    <h4>&nbsp;</h4>
+                    <ul>
+                      <li><a href="#">Biography</a></li>
+                      <li><a href="#">Astrology</a></li>
+                      <li><a href="#">Digital Marketing</a></li>
+                      <li><a href="#">Software Development</a></li>
+                      <li><a href="#">Ecommerce</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+                <div class="single-footer-caption mb-50">
+                  <div class="footer-tittle">
+                    <h4>Site Map</h4>
+                    <ul class="mb-20">
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                      <li><a href="#">Register</a></li>
+                      <li><a href="#">Login</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="footer-bottom-area">
+          <div class="container">
+            <div class="footer-border">
+              <div class="row d-flex align-items-center">
+                <div class="col-xl-12">
+                  <div class="footer-copy-right text-center">
+                    Copyright &copy; BookBridge
+                    <script>
+                      document.write(new Date().getFullYear());
+                    </script>
+                    All rights reserved.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/price_rangs.js"></script>
+
+    <script src="assets/js/contact.js"></script>
+    <script src="assets/js/jquery.form.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/mail-script.js"></script>
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
